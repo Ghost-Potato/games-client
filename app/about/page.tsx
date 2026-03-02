4fe44988eebe8089b6f6539ac1350ec80afad3f4
Ghost-Potato <jemacdon@lakeheadu.ca>
2026-03-01 20:45:33 -0500

    init commit

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <main>
+            <h1>About This Site</h1>
+            <p>We're building this for COMP2068G in Winter 2026 @ LU</p>
+        </main>
+    )
+}
